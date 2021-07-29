3518bd063bb71a19ed850b05a453fedb3790bb84
mateib47 <m.bucur@student.utwente.nl>
2021-07-29 21:04:47 +0300

    the book object is logged to console

--- script.js
+++ script.js
@@ -3,3 +3,27 @@
 then updates the page accordingly(Matei's Book List)
 -add more options to the form(rate the book, save your favourite quote,etc)
 */
+
+let bookList = [];
+
+function addBook(text){
+  const book = {
+    id : Date.now(),
+    text,
+    finished : false
+  }
+  bookList.push(book);
+  console.log(bookList);
+}
+
+const form = document.querySelector('.js-form');
+form.addEventListener('submit',event => {
+  event.preventDefault();
+  const input = document.querySelector('.book-input');
+  const text = input.value.trim();
+  if(text !== ''){
+    addBook(text);
+    input.value = '';
+    input.focus();
+  }
+});
