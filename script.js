6fdd03f135c8fc6481f6d4a84d27d9a40612d5a5
mateib47 <m.bucur@student.utwente.nl>
2021-07-29 19:46:55 +0300

    did the initial frontend page

---
+++ script.js
@@ -0,0 +1,5 @@
+/* TODO:
+-add a popup that asks for your name and
+then updates the page accordingly(Matei's Book List)
+-add more options to the form(rate the book, save your favourite quote,etc)
+*/
