c4dcffd8bff4e029f35bc2eb79177744d6521b22
mateib47 <m.bucur@student.utwente.nl>
2021-07-30 23:41:36 +0300

    the book items are rendered + added a bit of css for them

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ then updates the page accordingly(Matei's Book List)
 
 let bookList = [];
 
+function renderBook(book) {
+  const list = document.querySelector('.book-list-js');
+  const isFinished = book.finished ? "finished" : "";
+  const node = document.createElement('li');
+  node.setAttribute('class', `book-item ${isFinished}`);
+  node.setAttribute('data-key', book.id);
+  node.innerHTML = `
+  <label for="${book.id}" class="thick-js thick"></label>
+  <input id="${book.id}" type="checkbox" class="check-box"/>
+  <p class="book-title">${book.text}</p>
+  <button class="delete-button">Delete</button>
+  `;
+  list.append(node);
+}
+
 function addBook(text){
   const book = {
     id : Date.now(),
@@ -13,7 +28,7 @@ function addBook(text){
     finished : false
   }
   bookList.push(book);
-  console.log(bookList);
+  renderBook(book);
 }
 
 const form = document.querySelector('.js-form');
