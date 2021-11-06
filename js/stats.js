971c9fb8afbb5bda47cb7eabcd0dc4786e943d96
mateib47 <m.bucur@student.utwente.nl>
2021-11-06 23:57:42 +0100

    added chart.js and added the genre pie chart

--- js/stats.js
+++ js/stats.js
@@ -1,5 +1,41 @@
 let totalPages = 0;
 let totalBooks = 0;
+let genresCount = [];
+
+function genresPie(){
+  var xValues = ["Scientific","Self-help", "Biography", "Historical", "Fiction", "Philosophy","Fantasy", "Scifi","Romance", "Other"];
+  var yValues = genresCount.map(x -> x.count);
+  var barColors = [
+    "#f2d90e",
+    "#d47b4a",
+    "#c68767",
+    "#684132",
+    "#9d6e5e",
+    "#c8b5aa",
+    "#d8853f",
+    "#d4e2b3",
+    "#f9e3b7",
+    "#3a2e27"
+  ];
+
+  new Chart("genrePieChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Your most read genres"
+      }
+    }
+  });
+
+}
 
 function setTotalPages(num){
   totalPages = num;
@@ -40,6 +76,7 @@ function renderStats(){
     = `Total pages read: ${getTotalPages()}`;
   document.getElementById('total-books').innerHTML
       = `Total books read: ${getTotalBooks()}`;
+  genresPie();
 }
 
 window.onload = updateStats();
