a9f9974bb63778b73111b0d325603c409b9f8a91
mateib47 <m.bucur@student.utwente.nl>
2021-11-13 19:59:46 +0100

    finished autocomplete function

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -44,26 +44,32 @@ async function autocomplete(inp) {
       a.setAttribute("id", this.id + "-autocomplete-list");
       a.setAttribute("class", "autocomplete-items");
       document.querySelector('#autocomplete-div').appendChild(a);
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < 5 && suggestions[i]; i++) {
         let b = document.createElement("div");
-        b.innerHTML = "<p>" + suggestions[i].volumeInfo.title + "</p>";
+        b.innerHTML = "<p>" + suggestions[i].volumeInfo.title + " by "+suggestions[i].volumeInfo.authors[0] + "</p>";
+        b.innerHTML += "<input type='hidden' value='" +i + "'>";
+        b.addEventListener('click', function(e) {
+          let index = this.getElementsByTagName("input")[0].value;
+          fillForm(index);
+          //console.log(suggestions[index]);
+          clearPrevious();
+        });
         a.appendChild(b);
       }
-    }, 5000);
-
+    }, 500);
   });
 }
+
+function fillForm(i) {
+  document.getElementById('title-input').value = suggestions[i].volumeInfo.title;
+  document.getElementById('author-input').value = suggestions[i].volumeInfo.authors[0];
+  document.getElementById('genre-input').value = suggestions[i].volumeInfo.categories[0];
+  document.getElementById('pages-input').value = suggestions[i].volumeInfo.pageCount;
+  document.getElementById('api-id').value = suggestions[i].id;
+}
+
 function clearPrevious() {
   document.querySelector('#autocomplete-div').innerHTML = '';
 }
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
-  });
-}
-
-
 
 autocomplete(document.querySelector('#title-input'));
