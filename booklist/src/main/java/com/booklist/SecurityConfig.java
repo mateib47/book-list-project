87621acba8916b7f5524c08cd7a322298745c50d
mateib47 <m.bucur@student.utwente.nl>
2021-11-21 23:00:07 +0100

    started to work on authentication

---
+++ booklist/src/main/java/com/booklist/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.booklist;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().antMatcher("/**").authorizeRequests()
+                .antMatchers("/","index.html").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login().permitAll()
+                .and().
+                logout().logoutSuccessUrl("/");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication().withUser("user").password("password").roles("USER")
+                .and().withUser("admin").password("password").roles("USER", "ADMIN");
+    }
+}
