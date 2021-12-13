6827c034bf3155fe094dade7a2b574e7c3c3dc55
mateib47 <m.bucur@student.utwente.nl>
2021-12-13 10:38:25 +0100

    add app user

---
+++ booklist/src/main/java/com/booklist/appuser/AppUserRepository.java
@@ -0,0 +1,13 @@
+package com.booklist.appuser;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+    Optional<AppUser> findByEmail(String email);
+
+}
