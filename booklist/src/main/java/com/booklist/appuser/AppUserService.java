a856d00d4e87388c4eb0ccb7c146d4cb063ec810
mateib47 <m.bucur@student.utwente.nl>
2021-12-20 22:59:07 +0100

    add confirmation token

--- booklist/src/main/java/com/booklist/appuser/AppUserService.java
+++ booklist/src/main/java/com/booklist/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.booklist.appuser;
 
+import com.booklist.registration.token.ConfirmationToken;
+import com.booklist.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +19,7 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email)
@@ -31,6 +37,15 @@ public class AppUserService implements UserDetailsService {
         String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
         appUser.setPassword(encodedPassword);
         appUserRepository.save(appUser);
-        return "success user sinup";
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+            token,
+            LocalDateTime.now(),
+            LocalDateTime.now().plusMinutes(15),
+            appUser
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+        //email
+        return token;
     }
 }
