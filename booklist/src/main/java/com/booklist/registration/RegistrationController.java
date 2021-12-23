bb99137d4e162656eed75a3484283565db5839cc
mateib47 <m.bucur@student.utwente.nl>
2021-12-23 23:58:41 +0100

    start implementing email sending

--- booklist/src/main/java/com/booklist/registration/RegistrationController.java
+++ booklist/src/main/java/com/booklist/registration/RegistrationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
-    private RegistrationService registrationService;
+    private final RegistrationService registrationService;
 
     @PostMapping
     public String register(@RequestBody RegistrationRequest request){
